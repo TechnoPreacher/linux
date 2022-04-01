a02273c97fecfe1e13f39e618d0d327ea52e82b0
Nikolay Sulima <nik.nik.sulima@gmail.com>
2022-04-01 22:04:05 +0300

    homework #2

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo '<b>THIS IS INDEX.PHP from firstProject folder</b>';
+echo "</br>";
+echo "</br>";
+echo 'HOST: '. ($_SERVER["HTTP_HOST"]);
+echo "</br>";
+echo 'DOCUMENT ROOT: '.($_SERVER["DOCUMENT_ROOT"]);
+
+
+
+
+
